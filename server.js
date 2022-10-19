492b9190da7c5fed12f28901b2f4b56d58b0838d
vinieFortes <vinie4apps@gmail.com>
2022-10-19 13:28:45 -0300

    to deploy in heroku

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const
   express = require('express'),
+  cors = require('cors'),
   serveStatic = require('serve-static'),
   history = require('connect-history-api-fallback'),
   port = process.env.PORT || 5000
 
 const app = express()
 
+app.use(cors())
 app.use(history())
 app.use(serveStatic(__dirname + '/dist/spa'))
 app.listen(port)
